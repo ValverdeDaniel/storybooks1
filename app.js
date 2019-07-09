e7302659d11574bd069d28134af91bfbf2702a7e
Valverde Daniel <valverdedaniel44@gmail.com>
2019-07-09 13:41:49 -0600

    google auth

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const passport = require('passport');
+
+//load User Model
+require('./models/User');
 
 //passport config
 require('./config/passport')(passport);
 
 //load routes
-const auth = require('./routes/auth')
+const auth = require('./routes/auth');
+
+//load keys file
+const keys = require('./config/keys');
+
+//map global promises
+mongoose.Promise = global.Promise;
+
+//mongoose connect
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true
+})
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('It Works!');
 });
+
+app.use(cookieParser());
+
+//passport Middleware
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(session())
 
 //use routes
 app.use('/auth', auth)
--- routes/auth.js
+++ routes/auth.js
@@ -10,4 +10,18 @@ router.get('/google/callback',
     res.redirect('/dashboard');
   });
 
+  router.get('/verify', (req, res) => {
+    if(req.user) {
+      console.log(req.user);
+    } else {
+      console.log('not auth');
+      res.redirect('/');
+    }
+  });
+
+  router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
 module.exports = router;
