742aab1a494d6104805c75ae267dc336dc32eb2c
Valverde Daniel <valverdedaniel44@gmail.com>
2019-07-18 14:14:23 -0600

    finished template storybooks 1.00

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://danny:danny123@ds115931.mlab.com:15931/storybooks',
-  googleClientID: '270938475112-aafnj56p7l6qsgg8mfjvinvosl3en8e3.apps.googleusercontent.com',
-  googleClientSecret: 'wy2xVUoleUYALPHvuHHOHxe5'
+if(process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
